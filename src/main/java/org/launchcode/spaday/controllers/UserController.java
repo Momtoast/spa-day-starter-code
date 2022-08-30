1c703a7d5b4358ce57e8f630f5f3514b6f7ddcc3
Melissa Matos <melissamomtoast@gmail.com>
2022-08-29 20:41:15 -0400

    vanilla studio done

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -17,9 +17,12 @@ public class UserController {
     @PostMapping("add")
     public String processAddUserForm(Model model, @ModelAttribute User user, @RequestParam String verify) {
         if (verify.equals(user.getPassword())) {
+            model.addAttribute("error", false);
             return "serviceSelection";
         } else {
             model.addAttribute("error", true);
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
             return "user/add";
         }
     }
