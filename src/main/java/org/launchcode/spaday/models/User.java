a4c49f7d77759e8dcde314343ad67754badcbfb5
Melissa Matos <melissamomtoast@gmail.com>
2022-09-01 19:28:48 -0400

    last weeks studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,10 +1,19 @@
 package org.launchcode.spaday.models;
 
 public class User {
-
+    private int id;
+    private static int nextId = 1
     private String username;
     private String email;
     private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.id=nextId;
+        nextId++;
+    }
 
     public String getUsername() {
         return username;
