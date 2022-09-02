efe44723f607af22813ccb400dbae830b907f6c7
Melissa Matos <melissamomtoast@gmail.com>
2022-09-01 20:06:35 -0400

    with my own version of the verify password

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,23 +3,29 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors, Model model, String verify) {
         model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+
+        if (errors.hasErrors()) {
+            return "user/add";
+        }
+
         if (user.getPassword().equals(verify)) {
            return "user/index";
         }
